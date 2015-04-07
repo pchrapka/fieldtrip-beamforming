b3aa6649912563b628d51230c3880af436e72464
pchrapka <pchrapka@gmail.com>
2015-04-07 11:28:48 -0400

    updated the install scripts added a few dipole configurations added a folder for external packages

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # fieldtrip-beamforming
 Simple EEG beamforming setup with automatic depedency checks
+
+## How to get set up?
+
+### Default anatomical data
+
+The default [anatomical data](ftp://ftp.fieldtriptoolbox.org/pub/fieldtrip/tutorial/Subject01.zip) can be downloaded from the fieldtrip website. Download the file to /pathtoproject/myproject/anatomy. Unzip the file.
+
--- install.sh
+++ install.sh
@@ -1,6 +1,30 @@
-#!/bin/sh
+#!/bin/bash
 # Set up dependencies
 
-# Install openmeeg
-sudo apt-get install openmeeg-tools
+## Install openmeeg
+#sudo apt-get install openmeeg-tools
 
+OS=$(uname -m)
+echo $OS
+if [ "$OS" == "x86_64" ]
+then
+    DIR=linux64
+    URL=http://openmeeg.gforge.inria.fr/download/OpenMEEG-2.2.0-Linux64.amd64-gcc-4.3.2-OpenMP-static.tar.gz
+else
+    echo "Unknown OS"
+    exit 2
+fi
+
+PKGDIR=external/openmeeg/$DIR
+
+FILE=openmeeg.tar.gz
+sudo mkdir -p $PKGDIR
+sudo wget -O $PKGDIR/$FILE $URL
+
+CURDIR=$(pwd)
+cd $PKGDIR
+sudo tar --strip-components=1 -xzf $FILE
+cd $CURDIR
+
+## Download data
+# wget ftp://ftp.fieldtriptoolbox.org/pub/fieldtrip/tutorial/Subject01.zip

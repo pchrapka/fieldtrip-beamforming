8d3144b234af332eca4b0a449c5ed9d639b45e90
pchrapka <pchrapka@gmail.com>
2016-03-01 09:02:44 -0500

    fixed anatomy path

--- configs/MRIS01.m
+++ configs/MRIS01.m
@@ -14,7 +14,7 @@ cfg.ft_prepare_mesh.tissue = {'brain','skull','scalp'};
 cfg.ft_prepare_mesh.numvertices = [2000, 2000, 1000];
 % NOTE [3000, 2000, 1000] gives me an error: Mesh is self intersecting
 % MRI data
-cfg.mri_data = fullfile('anatomy','Subject01','Subject01.mri');
+cfg.mri_data = fullfile(srcdir,'..','anatomy','Subject01','Subject01.mri');
 
 save('MRIS01.mat','cfg');
 

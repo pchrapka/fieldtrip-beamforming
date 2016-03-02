d9381e7723843951a54a3a51922d9c919849461a
pchrapka <pchrapka@gmail.com>
2016-03-01 20:03:24 -0500

    fixed download script

--- download_data.sh
+++ download_data.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 DESTDIR=anatomy
+if [ ! -d "$DESTDIR" ]; then
+    mkdir $DESTDIR
+fi
+
 TMPFILE=`mktemp`
 wget ftp://ftp.fieldtriptoolbox.org/pub/fieldtrip/tutorial/Subject01.zip -O $TMPFILE
 unzip -d $DESTDIR/Subject01 $TMPFILE
